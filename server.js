751b444ee4527d18d5efd9f64dfdf26a5828c4c0
Vincent Fuertez <vfuertez@gmail.com>
2022-11-14 01:02:59 -0500

    Setting up database

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-
+const drinks = require('./models/drinks')
 const PORT = 3000
 
 app.get('/', (request, response) => {
     response.send(`Welcome to the Gitpub App!`)
 });
 
+app.get('/drinks', (request, response) => {
+    response.send(drinks)
+})
 
 app.listen(PORT, (request, response) => {
     console.log(`Listening to port ${PORT}`)
