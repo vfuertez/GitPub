4e21c4209487275dc6767e6168c03bf41674a3b2
Vincent Fuertez <vfuertez@gmail.com>
2022-11-17 00:34:41 -0500

    complete

--- server.js
+++ server.js
@@ -6,22 +6,34 @@ const food = require('./models/food')
 const PORT = 3000;
 
 
+app.get('/', (request, response) => {
+    response.send(`Welcome to the Gitpub App!`)
+});
 
-app.get('/drinks/:id', (request, response) => {
-    response.send(request.params.id)
-})
 
-
+// Index
 app.get('/drinks', (request, response) => {
-    response.render("drinks_show.ejs", {
+    response.render("drinks_index.ejs", {
         allDrinks: drinks,
         allFood: food
     })
 });
 
+app.get('/food/:id', (request, response) => {
+response.render('food_show.ejs', {
+    allFoods: food[request.params.id],
+    index: request.params.id
+})
+})
 
-app.get('/', (request, response) => {
-    response.send(`Welcome to the Gitpub App!`)
+
+// Show route
+app.get('/drinks/:id', (request, response) => {
+    response.render('drinks_show2.ejs', {
+        allDrinks: drinks[request.params.id],
+        index: request.params.id
+    }
+    )
 });
 
 
